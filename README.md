e9dc1f85d52c4e479b1e5db7af0b261f573df55d
Farley Caesar <publicfarley@gmail.com>
2021-07-23 20:12:57 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FCDateUtilities
+
+A description of this package.
---
+++ Sources/FCDateUtilities/FCDateUtilities.swift
@@ -0,0 +1,6 @@
+public struct FCDateUtilities {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
